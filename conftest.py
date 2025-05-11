04d47af0807bc52b6d3e23a79d23198e77023180
griganas <anastasiagk5@yahoo.gr>
2025-05-11 05:57:05 +0300

    "Add support for Edge and Firefox in driver fixture"

--- conftest.py
+++ conftest.py
@@ -1,12 +1,31 @@
 import pytest
 from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browserName", action="store", default="chrome", help="browser selection"
+    )
+
+
 @pytest.fixture()
-def driver():
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'normal'
-    driver = webdriver.Chrome(options=options)
+def driver(request):
+    browserName = request.config.getoption("browserName")
+    if browserName == "chrome":
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+
+    elif browserName == "edge":
+        options = webdriver.EdgeOptions()
+        driver = webdriver.Edge(options=options)
+
+    elif browserName == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.page_load_strategy = 'normal'
+        driver = webdriver.Firefox(options=options)
+
     driver.implicitly_wait(4)
     driver.maximize_window()
+
     yield driver
     driver.quit()
